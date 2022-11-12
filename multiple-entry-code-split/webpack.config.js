827e502e995568ad8db1ebe736299889c2bdde33
chenzeqin <sailor_chenzeqin@163.com>
2022-11-12 14:33:16 +0800

    代码分割默认配置

--- multiple-entry-code-split/webpack.config.js
+++ multiple-entry-code-split/webpack.config.js
@@ -17,4 +17,29 @@ module.exports = {
     }),
   ],
   mode: 'production',
+  optimization: {
+    splitChunks: {
+      chunks:'all', // 激活默认配置，下面默认配置可以不写
+      // 默认配置
+      chunks: 'async',
+      minSize: 20000, // 单位bite
+      minRemainingSize: 0,
+      minChunks: 1,
+      maxAsyncRequests: 30, // 最大加载30个文件
+      maxInitialRequests: 30,
+      enforceSizeThreshold: 50000, // 强制要求超过50kb,一定会被打包
+      cacheGroups: {
+        defaultVendors: {
+          test: /[\\/]node_modules[\\/]/,
+          priority: -10,
+          reuseExistingChunk: true,
+        },
+        default: {
+          minChunks: 2,
+          priority: -20,
+          reuseExistingChunk: true,
+        },
+      },
+    },
+  },
 };
