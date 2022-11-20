faf21eda310350049b10a6fc624bdf7495574aec
chenzeqin <sailor_chenzeqin@163.com>
2022-11-20 17:04:53 +0800

    react-cli(pref): 修改合并配置后打包文件

--- react-cli/dist/static/js/About-18ea312f72.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkreact_cli=self.webpackChunkreact_cli||[]).push([[74],{404:function(e,t,n){n.r(t),n.d(t,{default:function(){return c}});var u=n(294);function c(){return u.createElement("div",null,"About")}}}]);
-//# sourceMappingURL=About-18ea312f72.js.map
---
+++ react-cli/dist/static/js/chuck-About-a72b9d50df.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkreact_cli=self.webpackChunkreact_cli||[]).push([[74],{429:function(e,t,n){n.r(t),n.d(t,{default:function(){return c}});var u=n(294);function c(){return u.createElement("div",null,"About page")}}}]);
